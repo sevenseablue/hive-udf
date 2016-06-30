5456701c78adbc4283b3b7a5ff521598895a7f13
sevenseablue <sevenseablue@gmail.com>
2016-06-30 15:47:04 +0800

    add touchparams

---
+++ src/main/java/udf/TouchParams.java
@@ -0,0 +1,14 @@
+package udf;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+import util.FlightLog;
+import util.FlightLogTouch;
+
+/**
+ * Created by seven on 24/06/16.
+ */
+public class TouchParams extends UDF {
+    public String evaluate(String action, String url, String post) {
+        return FlightLogTouch.getLogs(action, url, post).toHive();
+    }
+}
